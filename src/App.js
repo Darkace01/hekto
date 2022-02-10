565b36cc43cebc03c869a905050423e2d3f1ea82
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-02-10 16:09:54 +0100

    NavBar

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import { GlobalStyle } from "./GlobalStyle";
 
 //components
 import TopBar from "./component/TopBar";
+import NavBar from "./component/NavBar";
 
 function App() {
   return (
     <Router>
       <TopBar />
+      <NavBar />
       <div className="app">Hello World</div>
       <GlobalStyle />
     </Router>
