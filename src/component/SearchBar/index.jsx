3ed3a2be9b3a68539df3e683980ca7b72b3f79ca
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-02-11 07:59:36 +0100

    Search bar width

--- src/component/SearchBar/index.jsx
+++ src/component/SearchBar/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+//icon
 import { FaSearch } from "react-icons/fa";
+
+//styling
+import "./SearchBar.css";
 
 //components
 import CustomButton from "../CustomButton";
 
 const SearchBar = () => {
   return (
-    <form className="d-flex">
+    <form className="d-flex search-bar">
       <input
         className="form-control rounded-0 w-100"
         type="search"
